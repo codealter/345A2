b0875fd0d4f54ef68f93b30b856a12bd7fdb9d8a
codealter <imortalsoulz@gmail.com>
2014-11-10 16:06:20 -0500

    Updated Object and Identity

--- Identity.h
+++ Identity.h
@@ -2,14 +2,22 @@
 #include <stdio.h>
 #include "Object.h"
 #include <iostream>
+#include <string>
+#include "JSONReader.h"
 using namespace std;
-class Identity : Object{
+class Identity : public Object{
 public:
-	void set(){}
-	const std::string getDVS() const{
-		return "";
+	void set(List<NVPair<std::string, std::string>, DATA_MEMBERS_PER_OBJECT> l){}
+	
+	const std::string getDVS(char c = VALUE_DELIMITER ) const
+	{
+		return std::string();
+	}
+	~Identity()
+	{
+
 	}
 };
-ostream& operator<<(ostream&, const Identity&){
-	cout << "" << endl;
+ostream& operator<<(ostream& os, const Identity& i){
+	return os;
 }
--- Object.h
+++ Object.h
@@ -1,11 +1,14 @@
 #include "List.h"
+#include "NVPair.h"
+static const int DATA_MEMBERS_PER_OBJECT = 10;
+static const char VALUE_DELIMITER = ' ';
 class Object {
 public:
 	// set assigns values to the data members of the derived Object
-	virtual void set() = 0;
+	virtual void set(List<NVPair<std::string, std::string>, DATA_MEMBERS_PER_OBJECT>()) = 0;
 
 	// getDSV returns the values of the data members delimied by VALUE_DELIMITER 
-	virtual const std::string getDSV() const = 0;
+	virtual const std::string getDSV(char) const = 0;
 
 	// derived objects may contain pointers: virtual destructors are necessary
 	virtual ~Object() {}
